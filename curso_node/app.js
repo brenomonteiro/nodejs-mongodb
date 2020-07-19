5c1d085d594714382172a260e52f8dc045fec6d5
breno.monteiro <brennoo.mendesmonteiro@gmail.com>
2020-07-19 11:46:42 -0300

    refactoring das rotas com consign

--- curso_node/app.js
+++ curso_node/app.js
@@ -1,12 +1,12 @@
 var app =  require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias');
+/*var rotaNoticias = require('./app/routes/noticias');
 var rotaFormularioNoticia = require('./app/routes/formulario_inclusao_noticia');
 var rotaHome = require('./app/routes/home');
 
 rotaNoticias(app);
 rotaFormularioNoticia(app);
-rotaHome(app);
+rotaHome(app);*/
 
 app.listen(3000,function(){
     console.log('servidor com nodemon iniciado');
