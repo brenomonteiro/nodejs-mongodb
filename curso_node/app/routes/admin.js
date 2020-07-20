fc8bdad7d9e2c703df2a0c3f5db2d2fd678427aa
breno.monteiro <brennoo.mendesmonteiro@gmail.com>
2020-07-19 21:37:05 -0300

    usando o express validator no formulário

--- curso_node/app/routes/admin.js
+++ curso_node/app/routes/admin.js
@@ -1,12 +1,26 @@
 module.exports = function(app){
 
 app.get('/formulario_inclusao_noticia',function(req, res){
-    res.render('admin/form_add_noticia');
+    res.render('admin/form_add_noticia',{val:{},noticia:{}});
 });
 
 app.post('/noticias/salvar',function(req, res){
     var noticia = req.body;
     
+    req.assert('titulo','Titulo é obrigatório').notEmpty();
+    req.assert('resumo','Resumo é obrigatório').notEmpty();
+    req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+    req.assert('autor','Autor é obrigatório').notEmpty();
+    req.assert('data_noticia','Data é obrigatório').notEmpty().isDate({format: 'yyyy-MM-DD'});
+    req.assert('noticia','Noticia é obrigatório').notEmpty();
+
+    var erros = req.validationErrors();
+    if(erros){
+        
+        res.render('admin/form_add_noticia', {val : erros, noticia : noticia});
+        return;
+    }
+
     //res.render('admin/form_add_noticia');
 
     var connection = app.config.dbConnection();
