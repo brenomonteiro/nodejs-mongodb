78c623065563d9b42f08975883de4102ac764b84
breno.monteiro <brennoo.mendesmonteiro@gmail.com>
2020-08-04 21:28:05 -0300

    incluindo publicações no banco de dados

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var multiparty = require('connect-multiparty');
 var mongodb = require('mongodb');
 var ObjectId = require('mongodb').ObjectId;
+var fs = require('fs');//fili system
+
 var app = express();
 
 //bodyParser
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
+app.use(multiparty());
 var port = 80;
 app.listen(port);
 
@@ -28,19 +31,41 @@ app.get('/',function(req,res){
 
 //POST
 app.post('/api',function(req,res){
-    var dados = req.body;
-    db.open(function(err, mongoclient){
-        mongoclient.collection("postagens",function(err,collection){
-            collection.insert(dados,function(err, records){
-                if(err){
-                    res.json(err);
-                }else{
-                    res.send(records);
-                }
-                mongoclient.close();
+
+    res.setHeader("Access-Control-Allow-Origin","*");
+    var date = new Date();
+    var time_stamp = date.getTime();
+    var url_imagem = time_stamp+'_'+req.files.arquivo.originalFilename;
+
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/'+url_imagem;
+    
+    
+    fs.rename(path_origem,path_destino,function(err){
+        if(err){
+            res.status(500).json({error:err});
+            return;
+        }
+
+        var dados = {
+            url_imagem:url_imagem,
+            titulo: req.body.titulo
+        }
+        db.open(function(err, mongoclient){
+            mongoclient.collection("postagens",function(err,collection){
+                collection.insert(dados,function(err, records){
+                    if(err){
+                        res.json({'status':'erro'});
+                    }else{
+                        res.send({'status':'inclusão realizada com sucesso'});
+                    }
+                    mongoclient.close();
+                });
             });
         });
     });
+   
+    
     //res.send(dados);
 });
 
